0f548c3feef01952e49a98fe8a588cd0afa3499a
Noemi Ancari <noemiancari.na@gmail.com>
2026-01-04 20:05:48 -0400

    listado de productos

--- src/app/pages/products/product-list/product-list.ts
+++ src/app/pages/products/product-list/product-list.ts
@@ -9,57 +9,55 @@ import { SelectButton } from 'primeng/selectbutton';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../../../../service/ProductService';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
   selector: 'app-product-list',
   imports: [
     CommonModule,
+    FormsModule,
     CardModule,
     ButtonModule,
     DataView,
     Tag,
-
-    SelectButton,
-    FormsModule,
+    SelectButton
   ],
   schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
- layout: 'list' | 'grid' = 'grid';
 
-  products = signal<Product[]>([]);
+  layout: 'list' | 'grid' = 'grid';
+  options: ('list' | 'grid')[] = ['list', 'grid'];
 
-  options: string[]  = ['list', 'grid'] as const;;
+  products = signal<Product[]>([]); // señal vacía al inicio
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getProducts().then((data) => {
-      this.products.set([...data.slice(0, 12)]);
+    // Como getProductsData() devuelve un Promise
+     this.productService.getProductsData().subscribe((data) => {
+      this.products.set(data.slice(0, 12)); // guardamos los primeros 12 productos
     });
   }
 
-  get productList() {
-  return this.products();
-}
-
+  get productList(): Product[] {
+    return this.products(); // retorna los productos actuales
+  }
 
   getSeverity(product: Product) {
-    switch (product.inventoryStatus) {
-      case 'INSTOCK':
+    // Ajustado a los valores de tu backend
+    switch (product.inventoryState) {
+      case 'AVAILABLE':
         return 'success';
-
-      case 'LOWSTOCK':
-        return 'warn';
-
-      case 'OUTOFSTOCK':
+      case 'LOW_STOCK':
+        return 'warning';
+      case 'OUT_OF_STOCK':
         return 'danger';
-
       default:
-        return null;
+        return 'info';
     }
   }
 }
--- src/domain/product.model.ts
+++ src/domain/product.model.ts
@@ -1,12 +1,13 @@
+export type InventoryState = 'AVAILABLE' | 'LOW_STOCK' | 'OUT_OF_STOCK';
+
 export interface Product {
-    id?: string;
-    code?: string;
-    name?: string;
-    description?: string;
-    price?: number;
-    quantity?: number;
-    inventoryStatus?: string;
-    category?: string;
-    image?: string;
-    rating?: number;
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: string;
+  rating: number;
+  inventoryState: InventoryState;
 }
